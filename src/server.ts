f35ec7718722d273e6d54dbed696e05a591bdc7d
Raja Parsaulian B <raja.parsaulian@gmail.com>
2020-07-02 15:02:54 +0700

    ADD Nodemon so the server will restarted when file was edited

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import data from "./data/data";
 
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Typescript");
+  res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  console.log("App is started on port 3000!");
+app.get("/products", (req, res) => {
+  res.send(data.products);
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend server is started on port ${PORT}.`);
 });
