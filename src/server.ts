3433ce2388a88ba15e1d354322d9cba6dc6f4f5d
Raja Parsaulian B <raja.parsaulian@gmail.com>
2020-07-09 02:52:00 +0700

    Fix CORS issue

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
 import mongoose from "mongoose";
 import config from "./config";
 import data from "./data/data";
@@ -11,6 +12,10 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 mongoose
   .connect(config.MONGODB_URL, {
@@ -23,9 +28,6 @@ mongoose
 mongoose.connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
-
-app.use(cors());
-app.use(express.json());
 
 // API Routes
 app.use("/users", userRoute);
